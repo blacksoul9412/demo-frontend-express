3a5a544a70055c665bd56b840dd16d626b2d7998
Blacksoul9412 <kumarrishi9532@gmail.com>
2024-04-29 15:20:47 +0530

    added files

---
+++ routes/result.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import {reports} from '../data/reports.js';
+
+const router = new express.Router();
+
+router.get('/', function (req, res, next) {
+  res.send('This route only receives a form or XHR POST');
+});
+
+router.post('/', function (req, res, next) {
+  // If it's an AJAX request, respond with some JSON otherwise render the index page
+  if (req.xhr) {
+    res.status(200).send({});
+  } else {
+    res.render('index', reports);
+  }
+});
+
+export {router as resultRouter};
